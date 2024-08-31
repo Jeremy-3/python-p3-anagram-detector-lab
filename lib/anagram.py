fe9e5e0cbf5506e517652df7e943a240dba3abbf
+ <+>
2024-08-31 13:03:33 +0300

    test completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+        self.sorted_word = sorted(word.lower())
+    def match(self,possible_anagram):
+        matches = []
+        
+        for word in possible_anagram:
+            if sorted(word.lower()) == self.sorted_word:
+                matches.append(word)        
+
+        return matches    
+
+   
